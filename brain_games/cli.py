eea74df0204a0f875cfda42aa82617509a3f857a
Roman Strakhov <rvstrakhov@gmail.com>
2020-11-03 00:02:08 +0300

    update cli

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,5 +6,6 @@ import prompt
 def welcome_user():
     """Username prompt, user greet."""
     print('Welcome to the Brain Games!')  # noqa: WPS421
-    name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name.capitalize()))  # noqa: WPS421
+    user_name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(user_name.capitalize()))  # noqa: WPS421
+    return user_name.capitalize()
