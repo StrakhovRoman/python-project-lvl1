7e999dd680b408d9b44585e14857cdd988350d3a
Roman Strakhov <rvstrakhov@gmail.com>
2020-11-21 18:34:08 +0300

    update game_engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -2,25 +2,23 @@
 
 import prompt
 
-ROUND_COUNT = 3
+ROUNDS_COUNT = 3
 
 
-def run_game(game_rules, get_question_and_answer):
+def run(game):
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ').capitalize()
     print('Hello, {0}!'.format(user_name))
-    print(game_rules)
-    for _ in range(ROUND_COUNT):   # noqa: WPS122
-        question, correct_answer = get_question_and_answer()
+    print(game.DESCRIPTION)
+    for _ in range(ROUNDS_COUNT):   # noqa: WPS122
+        question, correct_answer = game.get_question_and_answer()
         print('Question: {0} '.format(question))
         user_answer = prompt.string('Your answer: ').lower()
         if user_answer == correct_answer:
             print('Correct!')
         else:
-            print(
-                """'{0}' is wrong answer ;(. Correct answer was '{1}'
-                """.format(user_answer, correct_answer),
-            )
+            wrong_answer = "'{0}' is wrong answer ;(. Correct answer was '{1}'"
+            print(wrong_answer.format(user_answer, correct_answer))
             print("Let's try again, {0}".format(user_name))
             break
     else:
